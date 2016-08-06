908c4008cf94bc449e0992f45db9f2d918fd3e56
EDWIN\Owner <gohbiscuit@gmail.com>
2016-08-06 14:54:15 +0800

    Library files commit

---
+++ PokemonGuide/Assets/Scripts/TestButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : MonoBehaviour 
+{
+	public void ClickTest()
+	{
+		Application.OpenURL ("https://fevgames.net/pokedex/");
+	}
+
+	public void ClickTest2()
+	{
+		Application.OpenURL ("https://support.pokemongo.nianticlabs.com/hc/en-us/articles/222049307-Glossary");
+	}
+}
